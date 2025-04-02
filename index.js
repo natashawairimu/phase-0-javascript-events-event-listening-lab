2417a191d1e0191bd95ee8ce6719c7bfb7d6bec2
natashanyokabi <natashawairimu27@gmail.com>
2025-04-03 01:53:16 +0300

    events

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+    let input = document.getElementById("button");
+    function addingEventListener() {
+      input.addEventListener("click", function () {
+        alert("I was clicked!");
+      });
+    }
+  
+    addingEventListener();
+  }
+
